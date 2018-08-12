3a6d21843946c934ffcfcfec21dc01d71a771aba
manmanma <1057899309@qq.com>
2018-08-12 15:41:00 +0800

    update login

--- src/containers/main/main.jsx
+++ src/containers/main/main.jsx
@@ -1,8 +1,9 @@
-import React,{Component} from 'react'
-export default class Main extends Component{
-  render(){
+import React,{Component} from 'react';
+
+export default class Main extends Component {
+  render() {
     return (
-      <div>Main</div>
+      <div>main</div>
     );
   }
 }
